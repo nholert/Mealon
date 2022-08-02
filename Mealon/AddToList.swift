52a86a2cb373b8a38bb22477d0c204361b0a7c83
Natalie <nataliemholert@gmail.com>
2022-08-02 16:24:33 -0400

    iteration 1a

---
+++ Mealon/AddToList.swift
@@ -0,0 +1,41 @@
+//
+//  AddToList.swift
+//  Mealon
+//
+//  Created by Scholar on 8/2/22.
+//
+
+import UIKit
+
+class AddToList: UIViewController {
+
+    @IBOutlet weak var mealName: UITextField!
+    
+    @IBOutlet weak var ingNeeded: UITextField!
+    
+    
+    @IBOutlet weak var date: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addToList(_ sender: Any) {
+        
+    }
+    //comment 
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
